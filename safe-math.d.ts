c5388282b4d5c9712ae4ec61c8d6113c7bdd4f6e
Dipankar <dipankarsaha1059@gmail.com>
2022-11-04 14:18:10 +0600

    safe-math.d.ts file recovered

---
+++ safe-math.d.ts
@@ -0,0 +1,13 @@
+export function add(...values: any[]): number;
+export function minus(...values: any[]): number;
+export function divide(...values: any[]): number;
+export function multiply(...values: any[]): number;
+export function mean(...values: any[]): number;
+export function median(...values: any[]): number;
+export function mode(...values: any[]): number;
+export function range(...values: any[]): number;
+export function percent(value: any): number;
+export function power(value: any, exponent?: any): number;
+export function reciprocal(value: any): number;
+export function square(value: any): number;
+export function sqrt(value: any): number;
